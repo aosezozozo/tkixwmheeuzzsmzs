344ded02e01b662244ae7805b217e3192379dd36
onlyoyrao999 <onlyoyrao@gmail.com>
2026-09-11 13:46:58 +0800

    Update cf_scanner_sync.py

--- scripts/cf_scanner_sync.py
+++ scripts/cf_scanner_sync.py
@@ -179,7 +179,7 @@ def main():
     else:
         print(f"Target Regions dynamically set to: {target_regions}")
     
-    check_api_url = "https://proxyip.xxxxxxx.nyc.mn/check"
+    check_api_url = "https://proxyip.xxxxxxxx.nyc.mn/check"
     sync_count = SYNC_COUNT
     scan_count = SCAN_COUNT
     
